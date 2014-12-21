0b80d4ea295ffc019f3175df88634dc781b67b08
goran <gboban70@gmail.com>
2014-12-21 15:03:45 +0100

    updated comments regarding matrix multiplication operator %*%

--- cachematrix.R
+++ cachematrix.R
@@ -18,6 +18,10 @@
 # display cached inverse (if solved - i.e. cacheSolve is previously called on given matrix)
 # > m1$getinv()
 #
+# confirm that cacheSolve returns inverse matrix (must solve matrix m1 first)
+#
+# m1$get() %*% m1$getinv()
+#
 
 ## creates matrix with getters and setters and setinv, getinv methods for access to cached inverse of given matrix
 
